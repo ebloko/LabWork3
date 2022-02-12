6bed2526f47b68b8476d12e1054a729b76f2be87
une pomme <AstafevK@yandex.ru>
2022-02-12 18:14:51 +0300

    LabWork3

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static int Factorial(int i)
+        {
+            if (i < 0)
+            {
+                return -1;
+            }
+            if (i <= 1)
+            {
+                return 1;
+            }
+            return i * Factorial(i - 1);   
+        }
+
+        static void Main()
+        {
+            Console.Write("i -> ");
+            int i = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(i));
+        }
+
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static double Pow(double x, int n)
+        {
+            if (n == 0)
+                return 1;
+            if (n < 0)
+                return 1 / (Pow(x, -n));
+            return x * Pow(x, n - 1);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("введите число х-");
+            double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("введите число н-");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(Pow(x, n));
+        }
+    }
+}
